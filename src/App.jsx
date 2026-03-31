6607a2a782a9040e21a73ac9d0738c77f954e4f6
Noah Balogun <meraki.servnbar@gmail.com>
2026-03-31 18:35:34 -0400

    qr code in admin dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import EventPage from "./pages/guest/EventPage";
 import BartenderPage from "./pages/bartender/BartenderPage";
 import QRCodeDisplay from "./components/QRCodeDisplay";
 import AdminPage from "./pages/admin/AdminPage";
-import MenuPage from "./pages/guest/MenuPage";
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
       <Route path="/event/:eventId" element={<EventPage />} />
       <Route path="/bartender" element={<BartenderPage />} />
       <Route path="/admin" element={<AdminPage />} />
-      <Route path="/menu" element={<MenuPage />} />
     </Routes>
   );
 }
