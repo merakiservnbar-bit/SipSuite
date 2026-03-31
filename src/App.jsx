d90c15beefd716b9ec85ec8507c9c1cfcf5f38ca
Noah Balogun <meraki.servnbar@gmail.com>
2026-03-31 15:50:02 -0400

    fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import EventPage from "./pages/guest/EventPage";
+import BartenderPage from "./pages/bartender/BartenderPage";
 import QRCodeDisplay from "./components/QRCodeDisplay";
 
 function App() {
-  return <QRCodeDisplay eventId="IW2uiDADYDBUfJGY3cRg" />;
+  return (
+    <Routes>
+      <Route path="/" element={<QRCodeDisplay eventId="IW2uiDADYDBUfJGY3cRg" />} />
+      <Route path="/event/:eventId" element={<EventPage />} />
+      <Route path="/bartender" element={<BartenderPage />} />
+    </Routes>
+  );
 }
 
 export default App;
