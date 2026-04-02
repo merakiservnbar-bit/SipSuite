42dbef691dd282d3cc87ec8596721ec1f65d13c9
Noah Balogun <meraki.servnbar@gmail.com>
2026-04-02 00:43:56 -0400

    variant defined

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -1,11 +1,11 @@
-import logo from "../../assets/sipsuite-logo-full.png";
+import full from "../../assets/sipsuite-logo-full.png";
 import icon from "../../assets/sipsuite-icon-only.png";
 import wordmark from "../../assets/sipsuite-wordmark-only.png"
 import flat from "../../assets/sipsuite-flat.png"
 
-export default function Logo({ type = "full", size = 40 }) {
+export default function Logo({ variant = "full", size = 40 }) {
   const map = {
-    logo,
+    full,
     icon,
     wordmark,
     flat
